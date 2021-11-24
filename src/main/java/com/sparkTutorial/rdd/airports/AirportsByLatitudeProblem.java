4de722727f7cb1b88b53f44eb5ab39b345a5b650
Binh Nguyen <binhnguyen@Binhs-MBP-M1.local>
2021-11-23 23:03:57 -0500

    complete airport ex

--- src/main/java/com/sparkTutorial/rdd/airports/AirportsByLatitudeProblem.java
+++ src/main/java/com/sparkTutorial/rdd/airports/AirportsByLatitudeProblem.java
@@ -1,5 +1,13 @@
 package com.sparkTutorial.rdd.airports;
 
+import com.sparkTutorial.rdd.commons.Utils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.codehaus.janino.Java;
+import scala.util.parsing.combinator.testing.Str;
+
 public class AirportsByLatitudeProblem {
 
     public static void main(String[] args) throws Exception {
@@ -16,5 +24,25 @@ public class AirportsByLatitudeProblem {
            "Tofino", 49.082222
            ...
          */
+
+//        Config
+
+        SparkConf conf = new SparkConf().setAppName("airports_latitude").setMaster("local[2]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+//        Read file
+        JavaRDD<String> airports = sc.textFile("in/airports.text");
+
+//        Filter it out
+        JavaRDD<String> airports_40 = airports.filter(line -> Float.valueOf (line.split(Utils.COMMA_DELIMITER)[6]) > 40);
+
+//        Map
+        JavaRDD<String> airports_by_latitude = airports_40.map(line ->
+                {
+                    String[] splits = line.split (Utils.COMMA_DELIMITER);
+                    return StringUtils.join (new String []{splits[1],splits[6]}, ",");
+                }
+                );
+        airports_by_latitude.saveAsTextFile("out/airports_by_latitude");
     }
 }
--- src/main/java/com/sparkTutorial/rdd/airports/AirportsInUsaProblem.java
+++ src/main/java/com/sparkTutorial/rdd/airports/AirportsInUsaProblem.java
@@ -1,5 +1,12 @@
 package com.sparkTutorial.rdd.airports;
 
+import com.sparkTutorial.rdd.commons.Utils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.util.parsing.combinator.testing.Str;
+
 public class AirportsInUsaProblem {
 
     public static void main(String[] args) throws Exception {
@@ -16,5 +23,38 @@ public class AirportsInUsaProblem {
            "Dowagiac Municipal Airport", "Dowagiac"
            ...
          */
+//        Configure the work
+        SparkConf conf = new SparkConf().setAppName("airports").setMaster("local[2]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+//        Read the file
+        JavaRDD<String> airports = sc.textFile("in/airports.text");
+
+//        Filter it out
+        JavaRDD<String> airports_USA = airports.filter(line -> line.split (Utils.COMMA_DELIMITER)[3].equals("\"United States\""));
+
+
+        JavaRDD<String> airport_city_name = airports_USA.map(line->
+                {
+                    String[] splits = line.split(Utils.COMMA_DELIMITER);
+                    return StringUtils.join(new String[]{splits[1], splits[2]}, ",");
+                }
+                );
+
+        airport_city_name.saveAsTextFile("out/airport_city_name.text");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
